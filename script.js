efed1d3ae226f3b3af6b670436d912057cf91979
Jake Honschopp <Jakehonschopp@yahoo.com>
2022-01-08 11:43:25 -0500

    adding in comments for easier readability

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+
+// here's where we select all the items we need to in the HTML
 const getMealBtn = document.getElementById('get_meal');
 const mealContainer = document.getElementById('meal');
 
+
+// adding an event listener for the click event, so that we can fetch the info 
+// from the database once it is clicked 
+// it will return the meals array, but with only one item in it, so we have to pass it on index
+// 0 into our next function
 getMealBtn.addEventListener('click', () => {
   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(res => res.json())
@@ -9,6 +16,9 @@ getMealBtn.addEventListener('click', () => {
   });
 });
 
+
+// this entire function's purpose is to get the JSON response, parse it, then transform it
+// into an HTML component that looks fine on our browser
 const createMeal = (meal) => {
   const ingredients = [];
   for(let i=1; i<=20; i++) {
@@ -18,7 +28,12 @@ const createMeal = (meal) => {
       break;
     };
   };
-  
+// we looped through the ingredients, 1-20 to check if the meal has the right ingredient-measure pair. 
+// if it does, we're putting it into the ingredients array. If there are no more ingredients we stop 
+// the loop with a "break"
+
+// Below, we are creating the newInnerHTML string which is going to hold all of the HTML markup. 
+// We are parsing the remaining properties that we want to be displayed on the page. 
   const newInnerHTML = `
     <div class="row">
       <div class="columns five">
@@ -28,7 +43,7 @@ const createMeal = (meal) => {
         ${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
         <h5>Ingredients:</h5>
         <ul>
-          ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+          ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')} 
         </ul>
       </div>
       <div class="columns seven">
@@ -46,9 +61,15 @@ const createMeal = (meal) => {
       </div>
     </div>` : ''}
   `;
-  
+// some of the properties might not have been available, and if that is the case we use the
+// ternary operator to check if we have the data to display the tag. If we don't have it then
+// we return an empty string, aka nothing shows up on the page. Category and area are examples of 
+// these properties. 
   mealContainer.innerHTML = newInnerHTML;
 };
+
+
+// function to fill the empty meals if there are none on the page load (there won't be any bc we don't save any)
 
 function fillEmptyMeals() {
   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
@@ -58,6 +79,9 @@ function fillEmptyMeals() {
   });
 };      
 
+
+// event listener that waits for the load, and if localStorage is empty (it always should be)
+// then we fire the fillEmptyMeals function from above
 window.addEventListener("load", function() { 
   console.log(localStorage.getItem("meals") === '[]');
   if(!localStorage.getItem("meals") || localStorage.getItem("meals") === '[]') {
